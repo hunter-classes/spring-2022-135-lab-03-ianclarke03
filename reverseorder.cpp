ed384afeca5ebf0468c3b9587b85c067e99de21e
ianclarke03 <83726782+ianclarke03@users.noreply.github.com>
2022-02-17 06:00:51 +0000

    d

---
+++ reverseorder.cpp
@@ -0,0 +1,11 @@
+/* For this task, add new files, reverseorder.cpp and reverseorder.h. You will have to modify the Makefile appropriately.
+
+In these files create a function void reverse_oreder(std::string date1, std::string date2). This function should print out the West basin elevation for all days in the interval in reverse chronological order.
+
+test from main
+*/
+
+void reverse_order(std::string date1, std::string date2)
+{
+
+}
